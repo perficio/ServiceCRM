231b124d577fa232ea58b2c616db94844dde944e
Eric Erhardt <eric.erhardt@gmail.com>
2016-12-12 18:44:18 -0600

    Add project files.

---
+++ ServiceCRM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceCRM.Startup))]
+namespace ServiceCRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
